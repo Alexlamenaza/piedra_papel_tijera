189a60f91ecdfadf6ceac27ef19b925c4bc719bf
luis.castrop <luis.castrop@cadena.com.co>
2021-10-25 10:47:19 -0500

    Agregar commit
    PPT-1

--- index.py
+++ index.py
@@ -6,3 +6,6 @@ suma=0
 for x in range(101):
     suma += x
 print("Como no ingresaste un valor correcto la suma predeterminada es 1..100 ",suma)
+print("hola")
+print("Como no ingresaste un valor correcto la suma predeterminada es 1..100 ",suma)
+print("hola")
