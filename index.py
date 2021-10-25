905bbf5a86d29e19ff45ddfe8824e195e823ac2c
luis.castrop <luis.castrop@cadena.com.co>
2021-10-25 09:34:03 -0500

    Agrego ciclo for
    PPT-1

--- index.py
+++ index.py
@@ -2,3 +2,7 @@
 name = "Ingrese nombre"
 lastname= "Ingrese el apellido"
 print("Ingrese su Nombre de Usuario")
+suma=0
+for x in range(101):
+    suma += x
+print("Como no ingresaste un valor correcto la suma predeterminada es 1..100 ",suma)
